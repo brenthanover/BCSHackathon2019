ff3281578909e1eb3ba66a03a1f622256c87b2bc
i1b9 <brenthanover@gmail.com>
2019-03-16 16:24:28 -0700

    get occupants, capacity

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -18,8 +18,8 @@ export default class Admin extends React.Component {
 
         this.state = {
             name: "asdf",
-            numOfOccupants: 0,
-            capacity: 20
+            occupants: 0,
+            capacity: 0
         }
 
         this.handleClick = this.handleClick.bind(this);
@@ -36,6 +36,22 @@ export default class Admin extends React.Component {
         this.firebase = firebase;
         this.db = firebase.database();
         this.setName();
+        this.setCapacity();
+        this.setOccupants();
+    }
+
+    setCapacity() {
+      this.firebase.database().ref(`/Shelters/Shelter1`).once('value').then((res) => {
+          let capacity = res.val().capacity;
+          this.setState({capacity});
+      });
+    }
+
+    setOccupants() {
+      this.firebase.database().ref(`/Shelters/Shelter1`).once('value').then((res) => {
+          let occupants = res.val().occupants;
+          this.setState({occupants});
+      });
     }
 
     setName() {
@@ -46,12 +62,12 @@ export default class Admin extends React.Component {
     }
 
     incCount() {
-        let numOfOccupants = this.state.numOfOccupants;
-        if (this.state.numOfOccupants >= this.state.capacity) {
+        let numOfOccupants = this.state.occupants;
+        if (this.state.occupants >= this.state.capacity) {
             // No!!!
         } else {
             numOfOccupants++;
-            this.setState({numOfOccupants: numOfOccupants});
+            this.setState({occupants: numOfOccupants});
             this.db.ref('Shelters/Shelter1').update({occupants: numOfOccupants})
                 .then(() => {
                     console.log("Success!");
@@ -60,12 +76,12 @@ export default class Admin extends React.Component {
     }
 
     decCount() {
-        let numOfOccupants = this.state.numOfOccupants;
-        if (this.state.numOfOccupants = 0) {
+        let numOfOccupants = this.state.occupants;
+        if (this.state.occupants <= 0) {
             // No!!!
         } else {
             numOfOccupants--;
-            this.setState({numOfOccupants: numOfOccupants});
+            this.setState({occupants: numOfOccupants});
             this.db.ref('Shelters/Shelter1').update({occupants: numOfOccupants})
                 .then(() => {
                     console.log("Success!");
@@ -79,8 +95,8 @@ export default class Admin extends React.Component {
         this.firebase.database().ref('/Shelters/Shelter1').once('value').then((res) => {
             console.log('Shelter name:', res.val().name);
             console.log('Shelter location:', res.val().location);
-            console.log('Shelter capacity:', res.val().numOfOccupants);
-            console.log('Current number of occupants:', res.val().numOfOccupants);
+            console.log('Shelter capacity:', res.val().capacity);
+            console.log('Current number of occupants:', res.val().occupants);
         });
     }
 
@@ -90,7 +106,8 @@ export default class Admin extends React.Component {
                 <Navbar/>
                 <h1>Admin Page</h1>
                 <p>Current Shelter: {this.state.name}</p>
-                <p>Current number of Occupants: {this.state.numOfOccupants}</p>
+                <p>Current number of Occupants: {this.state.occupants}</p>
+                <p>Current capacity: {this.state.capacity}</p>
                 <button onClick={() => this.incCount()}>Inc Count</button>
                 <button onClick={() => this.decCount()}>Dec Count</button>
             </div>
